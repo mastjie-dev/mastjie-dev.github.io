c20b257587ccede5743cd730f61b178b03075317
mastjie-dev <bountifulsquare4@gmail.com>
2024-09-03 22:00:48 +0800

    fix bugs

--- math/BoundingBox2D.js
+++ math/BoundingBox2D.js
@@ -1,47 +1,53 @@
 import Vector2 from './Vector2.js'
 
-class BoundingBox3D {
-    constructor(min, max) {
-        this.min = new Vector2().copy(min)
-        this.max = new Vector2().copy(max)
-    }
-
-    set(min, max) {
-        this.min.copy(min)
-        this.max.copy(max)
+class BoundingBox2D {
+    constructor() {
+        this.position = new Vector2()
+        this.min = new Vector2()
+        this.max = new Vector2()
     }
 
     intersectPoint(point) {
+        const min = new Vector2().copy(this.position).add(this.min)
+        const max = new Vector2().copy(this.position).add(this.max)
         if (
-            point.x >= this.min.x &&
-            point.x <= this.max.x &&
-            point.y >= this.min.y &&
-            point.y <= this.max.y
+            point.x >= min.x &&
+            point.x <= max.x &&
+            point.y >= min.y &&
+            point.y <= max.y
         ) return true
         return false
     }
 
     intersectBox(box) {
+        const tmin = new Vector2().copy(this.position).add(this.min)
+        const tmax = new Vector2().copy(this.position).add(this.max)
+        const bmin = new Vector2().copy(box.position).add(box.min)
+        const bmax = new Vector2().copy(box.position).add(box.max)
+
         if (
-            box.min.x <= this.max.x &&
-            box.max.x >= this.min.x &&
-            box.min.y <= this.max.y &&
-            box.max.y >= this.min.y
+            bmin.x <= tmax.x &&
+            bmax.x >= tmin.x &&
+            bmin.y <= tmax.y &&
+            bmax.y >= tmin.y
         ) return true
         return false
     }
 
     intersectCircle(circle) {
-        const x = Math.max(this.min.x, Math.min(circle.x, this.max.x))
-        const y = Math.max(this.min.y, Math.min(circle.y, this.max.y))
+        const min = new Vector2().copy(this.position).add(this.min)
+        const max = new Vector2().copy(this.position).add(this.max)
+        
+        const x = Math.max(min.x, Math.min(circle.position.x, max.x))
+        const y = Math.max(min.y, Math.min(circle.position.y, max.y))
 
         const distance = Math.sqrt(
-            (x-circle.x) * (x-circle.x) +
-            (y-circle.y) * (y-circle.y)
+            (x-circle.position.x) * (x-circle.position.x) +
+            (y-circle.position.y) * (y-circle.position.y)
         )
 
         return distance < circle.radius
     }
 }
 
-export default BoundingBox3D
+export default BoundingBox2D
--- math/BoundingCircle.js
+++ math/BoundingCircle.js
@@ -1,18 +1,9 @@
 import Vector2 from './Vector2.js'
 
 class BoundingCircle {
-    constructor(position, radius) {
-        this.position = new Vector2().copy(position)
+    constructor(radius) {
+        this.position = new Vector2()
         this.radius = radius
-    }
-
-    setPosition(x, y) {
-        this.position.x = x
-        this.position.y = y
-    }
-
-    setPositionVector(v2) {
-        this.position.copy(v2)
     }
 
     intersectPoint(point) {
@@ -21,29 +12,31 @@ class BoundingCircle {
             (point.y - this.position.y) * (point.y - this.position.y)
         )
 
-        return distance < this.radius.x
+        return distance < this.radius
     }
 
     intersectBox(box) {
-        const x = Math.max(box.x, Math.min(this.position.x, box.x))
-        const y = Math.max(box.y, Math.min(this.position.y, box.y))
+        const min = new Vector2().copy(box.position).add(box.min)
+        const max = new Vector2().copy(box.position).add(box.max)
+
+        const x = Math.max(min.x, Math.min(this.position.x, max.x))
+        const y = Math.max(min.y, Math.min(this.position.y, max.y))
 
         const distance = Math.sqrt(
             (x-this.position.x) * (x-this.position.x) +
             (y-this.position.y) * (y-this.position.y)
         )
 
-        return distance < circle.radius
+        return distance < this.radius
     }
 
     intersectCircle(circle) {
         const distance = Math.sqrt(
             (circle.position.x-this.position.x) * (circle.position.x-this.position.x) +
-            (circle.position.y-this.position.y) * (circle.position.y-this.position.y) +
-            (circle.position.z-this.position.z) * (circle.position.z-this.position.z)
+            (circle.position.y-this.position.y) * (circle.position.y-this.position.y)
         )
 
-        return distance < this.radius + circle.radius
+        return distance < (this.radius + circle.radius)
     }
 }
 
