c3ccaca8220a0ab0d8360c8e1d8563e78d0aed42
mastjie-dev <bountifulsquare4@gmail.com>
2024-09-09 12:10:38 +0800

    add load image bitmap

--- misc/utils.js
+++ misc/utils.js
@@ -7,7 +7,14 @@ function degreeToRadian(degree) {
     return degree * Math.PI / 180
 }
 
+async function loadImageBitmap(url) {
+    const res = await fetch(url)
+    const blob = await res.blob()
+    return await createImageBitmap(blob, { colorSpaceConversion: 'none' })
+}
+
 export {
     radianToDegree,
     degreeToRadian,
+    loadImageBitmap,
 }
