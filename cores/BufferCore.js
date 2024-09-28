0357c3577d6218782e31acc3cb1bde5335e91397
mastjie-dev <bountifulsquare4@gmail.com>
2024-09-28 17:59:12 +0800

    extends to multiples type buffers

--- cores/BufferCore.js
+++ cores/BufferCore.js
@@ -1,20 +1,20 @@
 
 class BufferCore {
-  constructor(name, type, data, options = {}) {
+  constructor(type, data) {
     this.isBuffer = true
     this.GPUBuffer = null
     this.data = data
 
-    this.name = name
+    this.name = ""
     this.type = type
     this.size = data.byteLength
     this.length = data.length
 
-    this.usage = options.usage || null
-    this.format = options.format || null
-    this.visibility = options.visibility || null
-    this.arrayStride = options.arrayStride || 0
-    this.mappedAtCreation = options.mappedAtCreation || false
+    this.usage = null
+    this.format = null
+    this.visibility = null
+    this.arrayStride = 0
+    this.mappedAtCreation = false
     this.offset = 0
     this.shaderLocation = 0
   }
@@ -23,14 +23,47 @@ class BufferCore {
     this.GPUBuffer = buffer
   }
 
-  setShaderLocation(num) {
-    this.shaderLocation = num
-    return this
-  }
-
   destroy() {
     this.GPUBuffer.destroy()
   }
 }
 
-export default BufferCore
+class VertexBuffer extends BufferCore {
+  constructor(name, data, format = "float32x3") {
+    super("vertex", data)
+    
+    this.name = name
+    this.format = format
+    this.arrayStride = format === "float32x3" ? 12 : 8
+    this.usage = GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+  }
+}
+
+class IndexBuffer extends BufferCore {
+  constructor(data, format = "uint32") {
+    super("index", data)
+
+    this.format = format
+    this.usage = GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST
+  }
+}
+
+class UniformBuffer extends BufferCore {
+  constructor(data) {
+    super("uniform", data)
+
+    this.usage = GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+    this.visibility = GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT
+  }
+}
+
+class StorageBuffer extends BufferCore {
+  constructor(data) {
+    super("storage", data)
+    
+    this.usage = GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+    this.visibility = GPUShaderStage.COMPUTE
+  }
+}
+
+export { BufferCore, VertexBuffer, IndexBuffer, UniformBuffer, StorageBuffer }
