c8d35a83e96e408bb215126960c91d1250417d81
mastjie-dev <bountifulsquare4@gmail.com>
2024-10-13 19:38:16 +0800

    add light group & remove shadow

--- scenes/Light.js
+++ scenes/Light.js
@@ -2,10 +2,26 @@ import BindGroup from "../cores/BindGroup.js"
 import BindGroupLayout from "../cores/BindGroupLayout.js"
 import Vector3 from "../math/Vector3.js"
 import NodeCore from './NodeCore.js'
-import BufferCore from "../cores/BufferCore.js"
-import VARS from "../cores/VARS.js"
+import { UniformBuffer } from "../cores/BufferCore.js"
 import Matrix4 from "../math/Matrix4.js"
-import { DepthTexture } from "../cores/TextureCore.js"
+
+class LightGroup {
+    constructor() {
+        this.isBind = false
+        this.lights = []
+    
+        this.bindGroup = new BindGroup()
+        this.bindGroupLayout = new BindGroupLayout()
+    }
+
+    add(light) {
+        this.lights.push(light)
+    }
+
+    traverse(callback) {
+        this.lights.forEach(callback)
+    }
+}
 
 class Light extends NodeCore {
     constructor() {
@@ -13,25 +29,13 @@ class Light extends NodeCore {
         this.isLight = true
 
         this.strength = 1
-        this.color = new Vector3(.6, .6, .8)
+        this.color = new Vector3(.8, .78, .8)
         this.target = new Vector3(0, 0, 0)
         this.projectionView = new Matrix4()
         this.viewSpacePosition = new Vector3()
-        this.viewSpaceTarget = new Vector3()
 
-        this.castShadow = true
-        this.shadowNearPlane = 1
-        this.shadowFarPlane = 100
-        this.shadowMapSize = 1024
-        this.shadowBias = 0.005
-        this.shadowDepthTexture = new DepthTexture(this.shadowMapSize, this.shadowMapSize)
-        this.shadowDepthTexture.format = "depth32float"
-
-        this.buffer = new BufferCore(
-            "light", "uniform", new Float32Array(28), VARS.Buffer.Uniform)
-
-        this.bindGroup = new BindGroup()
-        this.bindGroupLayout = new BindGroupLayout()
+        this.shadow = null
+        this.buffer = new UniformBuffer(new Float32Array(8))
     }
 
     updateViewSpacePosition(camera) {
@@ -42,10 +46,23 @@ class Light extends NodeCore {
     updateBuffer() {
         const arr = [
             ...this.viewSpacePosition.toArray(), 0,
-            ...this.color.toArray(), 0,
-            ...this.projectionView.elements,
-            this.strength, this.shadowBias, this.shadowMapSize, 0,     
+            ...this.color.toArray(), this.strength,
         ]
+        
+        if (this.shadow) {
+            if (this.buffer.data.length === 8) {
+                this.buffer.data = new Float32Array(32)
+                this.buffer.length = 32
+                this.buffer.size = 32 * 4
+            }
+
+            this.shadow.position.copy(this.position)
+            this.shadow.updateProjectionViewMatrix()
+            arr.push(
+                ...this.shadow.projectionViewMatrix.elements,
+                this.shadow.bias, this.shadow.mapSize, 0, 0,
+            )
+        }
         this.buffer.data.set(arr)
     }
 }
@@ -53,22 +70,8 @@ class Light extends NodeCore {
 class DirectionalLight extends Light {
     constructor() {
         super()
-
         this.dimension = 10
-    }
-
-    updateProjectionView() {
-        const d = this.dimension
-        const projection = new Matrix4()
-            .orthographic(-d, d, d, -d, this.shadowNearPlane ,this.shadowFarPlane)
-        
-        const view = new Matrix4()
-            .lookAt(this.position, this.target, new Vector3(0, -1, 0))
-
-        this.projectionView
-            .identity()
-            .multiplyMatrix(projection, view)
     }
 }
 
-export { Light, DirectionalLight }
+export { LightGroup, Light, DirectionalLight }
