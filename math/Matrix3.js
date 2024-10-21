8b6c1d085ba0ca9d3097a490cabdb9f89c591612
mastjie-dev <bountifulsquare4@gmail.com>
2024-10-21 10:21:47 +0800

    fix bug & add inverse

--- math/Matrix3.js
+++ math/Matrix3.js
@@ -19,14 +19,19 @@ class Matrix3 {
         return this 
     }
 
-    copy() {
+    copy(m1) {
         const el = this.elements
+        const m = m1.elements
 
         el[0] = m[0]; el[1] = m[1]; el[2]  = m[2];
         el[4] = m[4]; el[5] = m[5]; el[6]  = m[6];
         el[8] = m[8]; el[9] = m[9]; el[10] = m[10];
 
         return this
+    }
+
+    copyFromMatrix4(m1) {
+        this.copy(m1)
     }
 
     identity() {
@@ -67,9 +72,9 @@ class Matrix3 {
 
     translate(v2) {
         const a = [
-            1,    0,    0,
-            0,    1,    0,
-            v2.x, v2.y, 1,
+            1,    0,    0, 0,
+            0,    1,    0, 0,
+            v2.x, v2.y, 1, 0,
         ]
         this.multiplyMatrix(this.elements, a)
         return this
@@ -77,9 +82,9 @@ class Matrix3 {
 
     scale(v2) {
         const a = [
-            v2.x, 0,    0,
-            0,    v2.y, 0,
-            0,    0,    1,
+            v2.x, 0,    0, 0,
+            0,    v2.y, 0, 0,
+            0,    0,    1, 0,
         ]
         this.multiplyMatrix(this.elements, a)
         return this
@@ -89,9 +94,9 @@ class Matrix3 {
         const s = Math.sin(radian)
         const c = Math.cos(radian)
         const a = [
-            c,  s, 0,
-            -s, c, 0,
-            0,  0, 1
+            c,  s, 0, 0,
+            -s, c, 0, 0,
+            0,  0, 1, 0,
         ]
         this.multiplyMatrix(this.elements, a)
         return this
@@ -118,7 +123,44 @@ class Matrix3 {
         )
     }
 
-    inverse() {}
+    inverse() {
+        const m = this.elements
+
+        const m0  = m[5] * m[10] - m[9] * m[6]
+        const m1  = m[4] * m[10] - m[8] * m[6]
+        const m2  = m[4] * m[9]  - m[8] * m[5]
+        
+        const m4  = m[1] * m[10] - m[9] * m[2]
+        const m5  = m[0] * m[10] - m[8] * m[2]
+        const m6  = m[0] * m[9]  - m[8] * m[1]
+        
+        const m8  = m[1] * m[6]  - m[5] * m[2]
+        const m9  = m[0] * m[6]  - m[4] * m[2]
+        const m10 = m[0] * m[5]  - m[4] * m[1]
+        
+        const det = m[0]*m0 - m[4]*m4 + m[8]*m8
+
+        if (det === 0) {
+            this.clear()
+            return
+        }
+
+        const invDet = 1 / det
+
+        m[0]  = m0*invDet
+        m[1]  = -m4*invDet
+        m[2]  = m8*invDet
+
+        m[4]  = -m1*invDet
+        m[5]  = m5*invDet
+        m[6]  = -m9*invDet
+
+        m[8]  = m2*invDet
+        m[9]  = -m6*invDet
+        m[10] = m10*invDet
+
+        return this
+    }
 }
 
 export default Matrix3
