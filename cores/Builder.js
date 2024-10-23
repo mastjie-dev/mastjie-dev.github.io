82e043e04a5e3affb08c2daa19fa1257affeba1b
mastjie-dev <bountifulsquare4@gmail.com>
2024-10-23 22:51:26 +0800

    remove pipeline builder, refactor render pass descriptor builder

--- cores/Builder.js
+++ cores/Builder.js
@@ -1,120 +1,69 @@
 
-const PipelineDescriptorBuilder = {
-    descriptor: {},
-    start() {
-        this.descriptor = {}
-        return this
-    },
-    label(label) {
-        this.descriptor.label = label
-        return this
-    },
-    layout(pipelineLayout) {
-        this.descriptor.layout = pipelineLayout
-        return this
-    },
-    vertex(module, vertexBufferLayout, entryPoint = "main_vertex") {
-        this.descriptor.vertex = {
-            module,
-            entryPoint,
-            buffers: vertexBufferLayout,
+function clone() {
+    return {
+        colorAttachments: [{
+            view: null,
+            clearValue: [0, 0, 0, 0],
+            loadOp: "clear",
+            storeOp: "store"
+        }],
+        depthStencilAttachment: {
+            view: null,
+            depthClearValue: 1,
+            depthLoadOp: "clear",
+            depthStoreOp: "store",
+            stencilClearValue: 0,
+            stencilLoadOp: 'clear',
+            stencilStoreOp: 'store',
         }
-        return this
-    },
-    fragment(module, format, entryPoint = "main_fragment") {
-        this.descriptor.fragment = {
-            module,
-            entryPoint,
-            targets: [{ format }]
-        }
-        return this
-    },
-    primitive(cullMode, topology) {
-        this.descriptor.primitive = {
-            cullMode,
-            topology,
-        }
-        return this
-    },
-    depthStencil(depthWriteEnabled, depthFormat, depthCompare) {
-        this.descriptor.depthStencil = {
-            depthWriteEnabled,
-            format: depthFormat,
-            depthCompare
-        }
-        return this
-    },
-    end() {
-        return this.descriptor
+    }
+}
+
+function disableColorAttachment(descriptor) {
+    descriptor.colorAttachments = []
+}
+
+function disableDepthStencilAttachment(descriptor) {
+    descriptor.depthStencilAttachment = undefined
+}
+
+function disableDepth(descriptor) {
+    descriptor.depthStencilAttachment.depthClearValue = undefined
+    descriptor.depthStencilAttachment.depthLoadOp = undefined
+    descriptor.depthStencilAttachment.depthStoreOp = undefined
+}
+
+function disableStencil(descriptor) {
+    descriptor.depthStencilAttachment.stencilClearValue = undefined
+    descriptor.depthStencilAttachment.stencilLoadOp = undefined
+    descriptor.depthStencilAttachment.stencilStoreOp = undefined
+}
+
+function modifyColorAttachment(descriptor, key, value, index = 0) {
+    descriptor.colorAttachments[index][key] = value
+}
+
+function modifyDepthStencilAttachment(descriptor, key, value) {
+    descriptor.depthStencilAttachment[key] = value
+}
+
+function timestampWrites(descriptor, querySet) {
+    descriptor.timestampWrites = {
+        querySet: querySet.GPUQuerySet,
+        beginningOfPassWriteIndex: 0,
+        endOfPassWriteIndex: 1,
     }
 }
 
 const RenderPassDescriptorBuilder = {
-    descriptor: {},
-    start() {
-        this.descriptor = {
-            colorAttachments: [
-                {
-                    view: null,
-                    clearValue: [0, 0, 0, 0],
-                    loadOp: "clear",
-                    storeOp: "store",
-                }
-            ],
-            depthStencilAttachment: {
-                view: null,
-                depthClearValue: 1.,
-                depthLoadOp: "clear",
-                depthStoreOp: "store",
-                stencilClearValue: 0,
-                stencilLoadOp: 'clear',
-                stencilStoreOp: 'store',
-            },
-
-            //TODO: more options??
-        }
-        return this
-    },
-
-    disableColorAttachment() {
-        this.descriptor.colorAttachments = []
-        return this
-    },
-    disableDepthStencilAttachment() {
-        this.descriptor.depthStencilAttachment = undefined
-        return this
-    },
-    disableDepth() {
-        this.descriptor.depthStencilAttachment.depthClearValue = undefined
-        this.descriptor.depthStencilAttachment.depthLoadOp = undefined
-        this.descriptor.depthStencilAttachment.depthStoreOp = undefined
-        return this
-    },
-    disableStencil() {
-        this.descriptor.depthStencilAttachment.stencilClearValue = undefined
-        this.descriptor.depthStencilAttachment.stencilLoadOp = undefined
-        this.descriptor.depthStencilAttachment.stencilStoreOp = undefined
-        return this
-    },
-    modifyColorAttachment(key, value, index = 0) {
-        this.descriptor.colorAttachments[index][key] = value
-        return this
-    },
-    modifyDepthStencilAttachment(key, value) {
-        this.descriptor.depthStencilAttachment[key] = value
-        return this
-    },
-    timestampWrites(querySet) {
-        this.descriptor.timestampWrites = {
-            querySet: querySet.GPUQuerySet,
-            beginningOfPassWriteIndex: 0,
-            endOfPassWriteIndex: 1,
-        }
-        return this
-    },
-    end() {
-        return this.descriptor
-    }
+    clone,
+    disableColorAttachment,
+    disableDepthStencilAttachment,
+    disableDepth,
+    disableStencil,
+    modifyColorAttachment,
+    modifyDepthStencilAttachment,
+    timestampWrites
 }
 
-export { PipelineDescriptorBuilder, RenderPassDescriptorBuilder }
+export { RenderPassDescriptorBuilder }
