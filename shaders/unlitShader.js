e5a0e5507ce9705a508c0202b74368cc10a7821b
mastjie-dev <bountifulsquare4@gmail.com>
2024-10-28 00:02:59 +0800

    add scene bind group

--- shaders/unlitShader.js
+++ shaders/unlitShader.js
@@ -1,7 +1,12 @@
 const unlitShader = `
 struct VSOutput {
     @builtin(position) position: vec4f,
+    @location(1) normal: vec3f,
     @location(0) uv: vec2f,
+};
+
+struct Scene {
+    time: f32,
 };
 
 struct Camera {
@@ -15,8 +20,9 @@ struct Model {
 };
 
 @group(0) @binding(0) var<uniform> color: vec3f;
-@group(1) @binding(0) var<uniform> camera: Camera;
-@group(2) @binding(0) var<uniform> model: Model;
+@group(1) @binding(0) var<uniform> scene: Scene;
+@group(2) @binding(0) var<uniform> camera: Camera;
+@group(3) @binding(0) var<uniform> model: Model;
 
 @vertex fn main_vertex(
     @location(0) position: vec3f,
@@ -26,6 +32,7 @@ struct Model {
 {
     var output: VSOutput;
     output.position = camera.projection * camera.view * model.matrix * vec4f(position, 1.);
+    output.normal = normal;
     output.uv = uv;
     return output;
 }
@@ -35,7 +42,7 @@ struct Model {
 )
     -> @location(0) vec4f 
 {
-    return vec4f(color, 1.);
+    return vec4f(input.normal, 1.);
 }
 `
 
