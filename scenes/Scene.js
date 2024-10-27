98223c62107a4a60fb6b58c7905204281a221c85
mastjie-dev <bountifulsquare4@gmail.com>
2024-10-28 00:05:59 +0800

    build render & shadow strutures

--- scenes/Scene.js
+++ scenes/Scene.js
@@ -1,45 +1,85 @@
 import BindGroup from "../cores/BindGroup.js"
 import BindGroupLayout from "../cores/BindGroupLayout.js"
+import BaseMaterial from "./BaseMaterial.js"
+
+
 
 class Scene {
     constructor() {
-        this.tree = []
-        this.materialGroups = []
+        this.trees = []
         this.lights = []
+        this.materialGroups = []
+        this.shadowMaterialGroups = []
 
         this.bindGroup = new BindGroup()
         this.bindGroupLayout = new BindGroupLayout()
     }
 
-    addMesh(mesh) {
-        if (!mesh.parent) {
-            this.tree.push(mesh)
-        }
+    buildMaterialGroup(mesh) {
+        const material = mesh.material
+        const matGroup = this.materialGroups
+            .find(_ => _.material.uuid === material.uuid)
 
-        if (mesh.isMesh) {
-            const material = mesh.material
-            const materialGroup = this.materialGroups.find(_ => _.material.uuid === material.uuid)
-            if (!materialGroup) {
-                this.materialGroups.push({
-                    material,
+        if (matGroup) {
+            const pv = matGroup.primitives
+                .find(_ => _.geometry === mesh.geometry)
+
+            if (pv) {
+                pv.meshes.push(mesh)
+            }
+            else {
+                matGroup.primitives.push({
+                    geometry: mesh.geometry,
                     meshes: [mesh]
                 })
             }
-            else {
-                materialGroup.meshes.push(mesh)
-            }
         }
-
-        if (mesh.children.length) {
-            for (let child of mesh.children) {
-                this.add(child)
-            }
+        else {
+            this.materialGroups.push({
+                material,
+                primitives: [{
+                    geometry: mesh.geometry,
+                    meshes: [mesh]
+                }]
+            })
         }
     }
 
-    addLight(light) {
-        if (light.isLight) {
-            this.lights.push(light)
+    buildShadowMaterialGroup(mesh) {
+        const geometry = mesh.geometry
+        const primitive = this.shadowMaterialGroups
+            .find(_ => _.geometry === geometry)
+
+        if (primitive) {
+            primitive.meshes.push(mesh)
+        }
+        else {
+            this.shadowMaterialGroups.push({
+                geometry,
+                meshes: [mesh]
+            })
+        }
+    }
+
+    addNode(node) {
+        if (!node.parent) {
+            this.trees.push(node)
+        }
+
+        if (node.isMesh) {
+            this.buildMaterialGroup(node)
+            // TODO: add only mesh affected by light
+            this.buildShadowMaterialGroup(node)
+        }
+
+        if (node.isLight) {
+            this.lights.push(node)
+        }
+
+        if (node.children.length) {
+            for (let child of node.children) {
+                this.addNode(child)
+            }
         }
     }
 }
