1d479f2362c29eb53a61654055d33f70c74184ae
mastjie-dev <bountifulsquare4@gmail.com>
2024-10-28 21:59:03 +0800

    add SpotShadow class

--- scenes/Shadow.js
+++ scenes/Shadow.js
@@ -1,12 +1,13 @@
 import Vector3 from "../math/Vector3.js"
 import Matrix4 from "../math/Matrix4.js"
 import { DepthTexture } from "../cores/TextureCore.js"
+import { degreeToRadian } from "../misc/utils.js"
 
 class DirectionalShadow {
     constructor() {
         this.dimension = 40
         this.near = 1
-        this.far = 1000
+        this.far = 200
         this.mapSize = 1024
         this.bias = .007
         this.depthTexture = new DepthTexture(this.mapSize, this.mapSize)
@@ -32,4 +33,34 @@ class DirectionalShadow {
     }
 }
 
-export { DirectionalShadow }
+class SpotShadow {
+    constructor() {
+        this.fov = degreeToRadian(30)
+        this.aspect = 1
+        this.near = 1
+        this.far = 200
+        this.mapSize = 1024
+        this.bias = .007
+        this.depthTexture = new DepthTexture(this.mapSize, this.mapSize)
+        this.depthTexture.format = "depth32float"
+
+        this.position = new Vector3()
+        this.target = new Vector3()
+
+        this.projectionViewMatrix = new Matrix4()
+    }
+
+    updateProjectionViewMatrix(position, target) {
+        this.position.copy(position)
+        this.target.copy(target)
+
+        const viewMatrix = new Matrix4()
+        viewMatrix.lookAt(this.position, this.target, new Vector3(0, -1, 0))
+
+        this.projectionViewMatrix.clear()
+        this.projectionViewMatrix.perspective(this.fov, this.aspect, this.near, this.far)
+        this.projectionViewMatrix.multiply(viewMatrix)
+    }
+}
+
+export { DirectionalShadow, SpotShadow }
