0005fbd8a89a8aa62ba0773ab1e6ff18429a52d8
mastjie-dev <bountifulsquare4@gmail.com>
2024-10-31 18:07:53 +0800

    extend from mesh class & add normal matrix calculation

--- scenes/InstanceMesh.js
+++ scenes/InstanceMesh.js
@@ -1,42 +1,26 @@
-import BindGroup from "../cores/BindGroup.js"
-import BindGroupLayout from "../cores/BindGroupLayout.js"
-import BufferCore from "../cores/BufferCore.js"
-import VARS from "../cores/VARS.js"
+import { UniformBuffer } from "../cores/BufferCore.js"
 import Matrix4 from "../math/Matrix4.js"
+import Mesh from "./Mesh.js"
 
-
-class InstanceMesh {
-    constructor(geometry, material, count) {
-        this.name = ""
-        this.isInstanceMesh = true
-        this.geometry = geometry
-        this.material = material
+class InstanceMesh extends Mesh {
+    constructor(geometry, material, count, name = "") {
+        super(geometry, material, name)
         this.count = count
+        this.isInstanceMesh = true
 
-        this.worldMatrix = new Matrix4()
-        
         const instanceMatrixElements = count * 16
-        this.localMatrixArray = Array(instanceMatrixElements).fill(0)
-        this.normalMatrixArray = Array(instanceMatrixElements).fill(0)
-        
-        this.buffer = new BufferCore("instance mesh", "uniform",
-            new Float32Array(instanceMatrixElements), VARS.Buffer.Uniform)
-        
-        this.parent = null
+        this.instanceWorldMatrix = Array(instanceMatrixElements).fill(0)
+        this.instanceNormalMatrix = Array(instanceMatrixElements).fill(0)
 
-        this.bindGroup = new BindGroup()
-        this.bindGroupLayout = new BindGroupLayout()
-    }
-
-    addParent(parent) {
-        this.parent = parent
+        this.buffer = new UniformBuffer(new Float32Array(instanceMatrixElements * 2))
     }
 
     updateBuffer() {
-        this.buffer.data.set(this.localMatrixArray)
+        this.buffer.data.set(this.instanceWorldMatrix)
+        this.buffer.data.set(this.instanceNormalMatrix, this.count * 16)
     }
 
-    updateMatrix(index, matrix) {
+    updateInstanceMatrix(index, matrix) {
         const offset = index * 16
 
         if (this.parent) {
@@ -45,22 +29,44 @@ class InstanceMesh {
             this.worldMatrix.copy(matrix)
         }
 
-        this.localMatrixArray[offset  + 0] = this.worldMatrix.elements[0]
-        this.localMatrixArray[offset  + 1] = this.worldMatrix.elements[1]
-        this.localMatrixArray[offset  + 2] = this.worldMatrix.elements[2]
-        this.localMatrixArray[offset  + 3] = this.worldMatrix.elements[3]
-        this.localMatrixArray[offset  + 4] = this.worldMatrix.elements[4]
-        this.localMatrixArray[offset  + 5] = this.worldMatrix.elements[5]
-        this.localMatrixArray[offset  + 6] = this.worldMatrix.elements[6]
-        this.localMatrixArray[offset  + 7] = this.worldMatrix.elements[7]
-        this.localMatrixArray[offset  + 8] = this.worldMatrix.elements[8]
-        this.localMatrixArray[offset  + 9] = this.worldMatrix.elements[9]
-        this.localMatrixArray[offset + 10] = this.worldMatrix.elements[10]
-        this.localMatrixArray[offset + 11] = this.worldMatrix.elements[11]
-        this.localMatrixArray[offset + 12] = this.worldMatrix.elements[12]
-        this.localMatrixArray[offset + 13] = this.worldMatrix.elements[13]
-        this.localMatrixArray[offset + 14] = this.worldMatrix.elements[14]
-        this.localMatrixArray[offset + 15] = this.worldMatrix.elements[15]
+        this.instanceWorldMatrix[offset + 0] = this.worldMatrix.elements[0]
+        this.instanceWorldMatrix[offset + 1] = this.worldMatrix.elements[1]
+        this.instanceWorldMatrix[offset + 2] = this.worldMatrix.elements[2]
+        this.instanceWorldMatrix[offset + 3] = this.worldMatrix.elements[3]
+        this.instanceWorldMatrix[offset + 4] = this.worldMatrix.elements[4]
+        this.instanceWorldMatrix[offset + 5] = this.worldMatrix.elements[5]
+        this.instanceWorldMatrix[offset + 6] = this.worldMatrix.elements[6]
+        this.instanceWorldMatrix[offset + 7] = this.worldMatrix.elements[7]
+        this.instanceWorldMatrix[offset + 8] = this.worldMatrix.elements[8]
+        this.instanceWorldMatrix[offset + 9] = this.worldMatrix.elements[9]
+        this.instanceWorldMatrix[offset + 10] = this.worldMatrix.elements[10]
+        this.instanceWorldMatrix[offset + 11] = this.worldMatrix.elements[11]
+        this.instanceWorldMatrix[offset + 12] = this.worldMatrix.elements[12]
+        this.instanceWorldMatrix[offset + 13] = this.worldMatrix.elements[13]
+        this.instanceWorldMatrix[offset + 14] = this.worldMatrix.elements[14]
+        this.instanceWorldMatrix[offset + 15] = this.worldMatrix.elements[15]
+
+        const normalMatrix = new Matrix4()
+            .copy(this.worldMatrix)
+            .inverse()
+            .transpose()
+
+        this.instanceNormalMatrix[offset + 0] = normalMatrix.elements[0]
+        this.instanceNormalMatrix[offset + 1] = normalMatrix.elements[1]
+        this.instanceNormalMatrix[offset + 2] = normalMatrix.elements[2]
+        this.instanceNormalMatrix[offset + 3] = normalMatrix.elements[3]
+        this.instanceNormalMatrix[offset + 4] = normalMatrix.elements[4]
+        this.instanceNormalMatrix[offset + 5] = normalMatrix.elements[5]
+        this.instanceNormalMatrix[offset + 6] = normalMatrix.elements[6]
+        this.instanceNormalMatrix[offset + 7] = normalMatrix.elements[7]
+        this.instanceNormalMatrix[offset + 8] = normalMatrix.elements[8]
+        this.instanceNormalMatrix[offset + 9] = normalMatrix.elements[9]
+        this.instanceNormalMatrix[offset + 10] = normalMatrix.elements[10]
+        this.instanceNormalMatrix[offset + 11] = normalMatrix.elements[11]
+        this.instanceNormalMatrix[offset + 12] = normalMatrix.elements[12]
+        this.instanceNormalMatrix[offset + 13] = normalMatrix.elements[13]
+        this.instanceNormalMatrix[offset + 14] = normalMatrix.elements[14]
+        this.instanceNormalMatrix[offset + 15] = normalMatrix.elements[15]
     }
 }
 
